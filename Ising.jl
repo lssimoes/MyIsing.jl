a2541ef8b27c3525dc5d332a2725f64d5ca86121
Lucas Silva Simões <simoes.lucas.silva@gmail.com>
2014-07-08 22:40:43 -0300

    Updated wolff to matrices

--- Ising.jl
+++ Ising.jl
@@ -13,27 +13,34 @@ export randspin,
        spinneighbors,
        heatbath!,
        metropolis!,
-#       wolff!,
+       wolff!,
        phasediag
 
 
 include("heatbath.jl")
-#include("metropolis.jl")
-#include("wolff.jl")
+include("metropolis.jl")
+include("wolff.jl")
 
-randspin()                                       = 2rand(0:1)-1                         # To sort random spins
-magnetization(spinmatrix::Array{Int,2})          = abs(mean(spinmatrix))                # To calculate the total magnetization
-spingrid(n::Int)                                 = [randspin() for i in 1:n, j in 1:n]  # To generate a random spinmatrix
-flip!(spinmatrix::Array{Int, 2}, i::Int, j::Int) = spinmatrix[i, j] *= -1               # To flip a given spin
+####################################################################
+#                                                                  #
+# Generic Functions that independ of the convergence algorithm     #
+#                                                                  #
+####################################################################
 
-# Returns the spins of the neighbors of (i,j) at spinmatrix
-function spinneighbors(spinmatrix::Array{Int, 2}, i::Int, j::Int)
-    n = Array(Int, 0)
-    if i > 1 push!(n, spinmatrix[i-1, j]) end
-    if j > 1 push!(n, spinmatrix[i, j-1]) end
+randspin()                                               = 2rand(0:1)-1                         # To sort random spins
+magnetization(spinmatrix::Array{Int,2})                  = abs(mean(spinmatrix))                # To calculate the total magnetization
+spingrid(n::Int)                                         = [randspin() for i in 1:n, j in 1:n]  # To generate a random spinmatrix
+flip!(spinmatrix::Array{Int, 2}, i::Int, j::Int)         = spinmatrix[i, j] *= -1               # To flip a given spin
+spinneighbors(spinmatrix::Array{Int, 2}, i::Int, j::Int) = [spinmatrix[i,j] for (i,j) in neighbors(spinmatrix, i, j)]
 
-    if i < size(spinmatrix, 1) push!(n, spinmatrix[i+1, j]) end
-    if j < size(spinmatrix, 2) push!(n, spinmatrix[i, j+1]) end
+function neighbors(spinmatrix::Array{Int, 2}, i::Int, j::Int)
+    n = Array((Int, Int), 0)
+
+    if i > 1 push!(n, (i-1, j)) end
+    if j > 1 push!(n, (i, j-1)) end
+
+    if i < size(spinmatrix, 1) push!(n, (i+1, j)) end
+    if j < size(spinmatrix, 2) push!(n, (i, j+1)) end
 
     return n
 end
@@ -53,13 +60,13 @@ function phasediag(f::Function;
     it =  mintemp:step:maxtemp
 
     for i in it
-       push!(mα,tempdiag(f, size, i, qtd=ensembles, h=h, maxit=maxit))
+       push!(mα,meanontemp(f, size, i, qtd=ensembles, h=h, maxit=maxit))
     end
 
     if plot
         PyPlot.plot(it, mα, "-", color="red")
         PyPlot.title("Magnetization over Temperatures with " * "$f"[1:end-1])
-        PyPlot.savefig("Plots/" * "$f"[1:end-1] * "heatbath_$(n)grid_$(int(maxtemp))")
+        PyPlot.savefig("Plots/" * "$f"[1:end-1] * "_$(size)grid_$(int(maxtemp))")
         PyPlot.close()
     end
 
@@ -67,12 +74,13 @@ function phasediag(f::Function;
 end
 
 # Generic Algorithm that calculates a Function for many Graphs at a given Temperature
-function tempdiag(f::Function, 
-                  n::Int,
-                  temp::Float64;
-                  qtd::Int       = 200,
-                  h::Float64     = 0.0,
-                  maxit::Int     = 50000)
+function meanontemp(f::Function, 
+                    n::Int,
+                    temp::Float64;
+                    qtd::Int       = 200,
+                    h::Float64     = 0.0,
+                    maxit::Int     = 50000)
+
     mi = Array(Float64,0)
 
     for i in 1:qtd
--- wolff.jl
+++ wolff.jl
@@ -1,54 +1,59 @@
 # Step (RECURSIVE) of Wolff's Algorith
-function wolffstep!(pos::Int, g::GenericGraph; temp::Float64=1.0, h::Float64=0.0, maxit::Int=10000)
+function wolffstep!(spinmatrix::Array{Int, 2},
+                    i::Int, 
+                    j::Int; 
+                    temp::Float64 = 1.0, 
+                    h::Float64    = 0.0,
+                    maxit::Int   = 10000)
+
     if temp == 0 error("The Temperature can't be ZERO!!") end
 
-    pos_spin = g.vertices[pos].attributes["spin"]
-
     # Calculating neighbors' spin sum
-    mclose = sum([neighbor.attributes["spin"] for neighbor in out_neighbors(vertices(g)[pos],g)])
+    mclose = sum(spinneighbors(spinmatrix, x, y))
 
-    ΔE = 2(h+mclose)*pos_spin # energy necessary to flip the spin
-    if (ΔE < 0)
-        g.vertices[pos].attributes["spin"] *= -1 # flip spin
-    else 
-        # ELSE flip with Metropolis probability
-        prob_flip = exp(-ΔE/temp)
-        towersort = rand()
-        if towersort < prob_flip
-            g.vertices[pos].attributes["spin"] *= -1
-        end
+    ΔE = 2(h+mclose)*spinmatrix[x,y] # energy necessary to flip the spin
+    if ΔE < 0
+        flip!(spinmatrix, x, y) # flip spin certainly
+    elseif rand() < exp(-ΔE/temp)
+        flip!(spinmatrix, x, y) # flip spin with probability
     end
 
     # Recursive flip neighbor with same spin
-    for neighbor in out_neighbors(vertices(g)[pos],g)
-        if neighbor.attributes["spin"] == pos_spin
-            wolffstep!(pos,g,temp=temp, h=h, maxit=maxit)
+    for n in neighbors(spinmatrix, i, j)
+        if spinmatrix[n[1], n[2]] == spinmatrix[i,j]
+            wolffstep!(spinmatrix, n[1], n[2], temp=temp, h=h, maxit=maxit)
         end
     end
 end
 
 # Wolff's Algorithm
-function wolff!(g::GenericGraph; temp::Float64=1.0, h::Float64=0.0, maxit::Int=10000, plot::Bool=true)
-    # Inicializations
-    n = sqrt(num_vertices(g))
+function wolff!(spinmatrix::Array{Int, 2};
+                temp::Float64 = 1.0,
+                h::Float64    = 0.0,
+                maxit::Int    = 5000,
+                plot::Bool    = true,
+                verbose::Bool = true)
+
+    n = size(spinmatrix, 1)
     x , y = rand(1:n) , rand(1:n) # Generating the postion randomically
-    pos = n*(x-1)+y # Converting (x,y) to the position on the Vertices Array 
+    mi = Array(Float64, 0)
+    xi = 1:maxit
 
-    wolffstep!(pos,g,temp=temp, h=h, maxit=maxit)
+    for iter in xi
+        wolffstep!(spinmatrix, i, j, temp=temp, h=h, maxit=maxit)
+        push!(mi, abs(magnetization(spinmatrix)))
+    end
 
-    println("Finished with magnetization $(mi[end])")
+    if verbose println("Finished with magnetization $(mi[end])") end
     if plot
-        #PyPlot.plot(xi, mi, "o", color="blue")
-        PyPlot.plot(xi, mi, "-", color="blue")
-        PyPlot.ylim(0,1.2) # nao existia
-    
+        PyPlot.plot(xi, mi, "o", color="blue")
         PyPlot.title("Wolff on Ising for T=$temp")
         PyPlot.xlabel("Number of Iterations")
-        PyPlot.ylabel("Magnetization")
-        
+        PyPlot.ylabel("Magnetization")        
+        PyPlot.ylim(0,1.1)
         PyPlot.savefig("Plots/Wolff/wolff_mag_$temp.png")
         PyPlot.close()
-        # plot(x=xi,y=mi, Geom.point, Geom.line) #Gadfly
     end
+
     return mi[end]
 end
